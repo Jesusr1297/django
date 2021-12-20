9cef3b570edd9d2a4b7f54c6ef5e15175a9e9ffa
Jesus Romero <jesusromero@Jesuss-MacBook-Air.local>
2021-12-19 20:25:41 -0700

    added link that connects home with rooms, this by using a for loop to display a message from a list

--- base/views.py
+++ base/views.py
@@ -3,8 +3,23 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room_ = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room_ = i
+    context = {'room': room_}
+
+    return render(request, 'base/room.html', context)
+
+
+rooms = [
+    {'id': 1, 'name': 'lets learn some python'},
+    {'id': 2, 'name': 'design with me'},
+    {'id': 3, 'name': 'frontend developers '},
+
+]
