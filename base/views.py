36c385670e4c4df0eff96493d3012d770b87fe92
Jesus Romero <jesusromero@Jesuss-MacBook-Air.local>
2021-12-20 19:47:31 -0700

    Added more databases, using /admin

--- base/views.py
+++ base/views.py
@@ -1,25 +1,24 @@
 from django.shortcuts import render
+from .models import Room
 
 
 # Create your views here.
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
-    room_ = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room_ = i
-    context = {'room': room_}
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
 
     return render(request, 'base/room.html', context)
 
 
-rooms = [
-    {'id': 1, 'name': 'lets learn some python'},
-    {'id': 2, 'name': 'design with me'},
-    {'id': 3, 'name': 'frontend developers '},
-
-]
+# rooms = [
+#     {'id': 1, 'name': 'lets learn some python'},
+#     {'id': 2, 'name': 'design with me'},
+#     {'id': 3, 'name': 'frontend developers '},
+#
+# ]
