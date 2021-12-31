f26dabcd62959a62f1e3325596a8526a174e9b32
Jesus Romero <jesusr1297@outlook.com>
2021-12-31 07:23:53 -0700

    Added login page

--- base/views.py
+++ base/views.py
@@ -5,6 +5,11 @@ from .forms import RoomForm
 
 
 # Create your views here.
+def loginPage(request):
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(
