18ddcb1dd0643d23e98d1d8ce86576511fc868d6
Jesus Romero <jesusr1297@outlook.com>
2022-05-09 19:08:24 -0700

    Added edit user profile option. Fixed some bugs in navbar.html

--- base/urls.py
+++ base/urls.py
@@ -18,4 +18,6 @@ urlpatterns = [
     path('delete-room/<str:pk>', views.delete_room, name='delete-room'),
 
     path('delete-message/<str:pk>', views.delete_message, name='delete-message'),
+
+    path('update-user/', views.update_user, name='update-user'),
     ]
